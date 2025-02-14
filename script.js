8e243737f928a9c22c43fa5bf4a7f7c81a2d3177
Pixselschubserin <doroinperu@gmail.com>
2025-02-14 13:35:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const faqItems = document.querySelectorAll(".faq-item");
 
     faqItems.forEach((item) => {
-    const question = item.querySelector(".faq-question");
+    const question = item.querySelector("h3");
 
             question.addEventListener("click", () => {
                 item.classList.toggle("active");
